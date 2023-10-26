be452fa27c17c25836a3d6caa109a1749a6aded9
venkat3776 <kvenkat3776@gmail.com>
2023-10-26 21:18:56 +0530

    add ALL FILES

---
+++ SecondHandCar/src/test/java/com/project/SecondHandCar/SecondHandCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SecondHandCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
